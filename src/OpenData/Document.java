4dcfead7b1d09b7c65ee394f2064e0173d93c28d
Laurie <laurie.naejus@gmail.com>
2020-05-27 22:44:29 +0200

    all classes

--- src/OpenData/Document.java
+++ src/OpenData/Document.java
@@ -1,10 +1,62 @@
 package OpenData;
 
 public abstract class Document {
-	boolean dispo; //disponibilité du document
-	int numNotice; //numéro de notice
-	int ean; //numéro ean
-	boolean serie; //fait partie d'une série
-	String titre_serie; //si serie = 1, titre_serie = titre de la série auquel il appartient
+	
+	private boolean dispo; //disponibilité du document
+	private int numNotice; //numéro de notice
+	private int ean; //numéro ean
+	private boolean serie; //fait partie d'une série
+	private String titreSerie; //si serie = 1, titre_serie = titre de la série auquel il appartient
+	private int nbExemplaires; //nombre d'exemplaire du doc	
+	
+	public Document() {
+		dispo = true;
+		numNotice = 0;
+		ean = 0;
+		serie = false;
+		nbExemplaires = 1;
+	}
+	
+	public Document(int num, int ean, boolean serie, String titreS) {
+		dispo = true;
+		numNotice = num;
+		this.ean = ean;
+		this.serie = serie;
+		if(serie) titreSerie = titreS;
+	}
+	
+	public boolean getDispo() {
+		return dispo;
+	}
+	
+	public int getNumNotice() {
+		return numNotice;
+	}
+	
+	public int getEan() {
+		return ean;
+	}
+	
+	public boolean getSerie() {
+		return serie;
+	}
+	
+	public String getTitreSerie() {
+		return titreSerie;
+	}
+	
+	public int getNbExemplaires() {
+		return nbExemplaires;
+	}
+	
+	public void setDispo() {
+		dispo = !dispo;
+	}
+	
+	public void setNbExemplaires(int newNb) {
+		nbExemplaires = newNb;
+	}
 	
 }
+
+
