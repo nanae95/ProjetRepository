61d754faaf88cb3aac1739c2a57081f4fee6051d
Laurie <laurie.naejus@gmail.com>
2020-05-27 20:10:11 +0200

    all classes

--- src/OpenData/Document.java
+++ src/OpenData/Document.java
@@ -1,5 +1,10 @@
 package OpenData;
 
 public abstract class Document {
-
+	boolean dispo; //disponibilité du document
+	int numNotice; //numéro de notice
+	int ean; //numéro ean
+	boolean serie; //fait partie d'une série
+	String titre_serie; //si serie = 1, titre_serie = titre de la série auquel il appartient
+	
 }
