79db317384c6545076958258781523a67acdabee
Priscile <Priscile@LAPTOP-TAMAOGKT>
2020-05-28 19:36:42 +0200

    fonction echanger

--- src/OpenData/Bibliothèque.java
+++ src/OpenData/Bibliothèque.java
@@ -26,8 +26,12 @@ public class Bibliothèque {
 	
 	}
 	
-	public void echange(Bibliothèque b2, Document d1, Document d2) {
-		this.Bibliothèque.
+	//la fonction echanger permet à 2 bibliothèques de s'échanger 2 documents
+	public void echanger(Bibliothèque b2, Document d1, Document d2) {
+		listeDocs.supprimer(d1);
+		listeDocs.ajouter(d2);
+		b2.listeDocs.supprimer(d2);
+		b2.listeDocs.ajouter(d1);
 	}
 	
 	
