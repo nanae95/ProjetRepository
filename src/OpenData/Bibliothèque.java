fd8e554630a6ec5525e8933edb8442ea74b2c5c4
Priscile <Priscile@LAPTOP-TAMAOGKT>
2020-05-28 09:32:01 +0200

    bibliothèque commit

--- src/OpenData/Bibliothèque.java
+++ src/OpenData/Bibliothèque.java
@@ -4,7 +4,28 @@ import java.util.ArrayList;
 
 public class Bibliothèque {
 	private String nom;
-	private ArrayList <String> utilisateur;
+	private ArrayList <Utilisateur> listeInscrits;	
+	private ArrayList <Document> listeDocs;
+	//dans une bibliothèque besoin d'une liste de documents ?
 	
+	public Bibliothèque() {
+		nom = "";
+		ArrayList <Utilisateur> listeInscrits = new ArrayList<Utilisateur>();
+		ArrayList <Document> listeDoc = new ArrayList<Document>();
+	}
+	
+	public Bibliothèque(String nom, ArrayList <Utilisateur> listeInscrits, ArrayList <Document> listeDocs) {
+		this.nom = nom;
+		this.listeInscrits = listeInscrits;
+		this.listeDocs =listeDocs;
+		
+	}
+	
+	public void Inscrire(Utilisateur inscrit) {
+		listeInscrits.add(inscrit);
+	
+	}
 	
 }
+
+
